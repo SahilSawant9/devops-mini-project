a06a6cdf1a642f75e73f49f25c50ca834e314380
Sahil Santosh Sawant <sahilsawant0904@gmail.com>
2025-07-25 10:01:51 +0530

    ✅ Fixed Python import path in test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,13 @@
+# tests/test_app.py
+
 import app
+import sys
+import os
+
+# Ensure Python can find app.py during test
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 def test_home():
     response = app.app.test_client().get('/')
-    assert response.status_code == 200
     assert response.data == b'Majhe Naav Sahil Sawant'
