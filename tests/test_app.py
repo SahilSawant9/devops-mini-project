ddfd479a3a01bc81acfb8753a61a2423b82f4340
Sahil Santosh Sawant <sahilsawant0904@gmail.com>
2025-07-25 09:31:19 +0530

    ✅ Temporary: Run only tests in CI to fix exit code 2

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,4 +4,4 @@ import app
 def test_home():
     response = app.app.test_client().get('/')
     assert response.status_code == 200
-    assert response.data == b'Majhe Naav Sahil '
+    assert response.data == b'Majhe Naav Sahil Sawant'
