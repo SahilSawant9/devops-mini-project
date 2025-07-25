8ddacd92c05a4ca16f3e8bd7e145c1f7ac92bda1
Sahil Santosh Sawant <sahilsawant0904@gmail.com>
2025-07-25 11:14:31 +0530

    Update: Changed homepage response to only 'Sahil Sawant'

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Majhe Naav Sahil Sawant"
+    return 'Sahil Sawant'  # Only your name now
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0')
