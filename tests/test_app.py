166b431ddfcc04767144075bfccb519831c61d09
Sahil Santosh Sawant <sahilsawant0904@gmail.com>
2025-07-25 11:08:22 +0530

    Fix: final CI/CD without LocalStack

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,7 @@
-# tests/test_app.py
-
 import app
-import sys
-import os
-
-# Ensure Python can find app.py during test
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 def test_home():
     response = app.app.test_client().get('/')
-    assert response.data == b'Majhe Naav Sahil Sawant'
+    assert response.status_code == 200
+    assert response.data == b"Majhe Naav Sahil Sawant"
